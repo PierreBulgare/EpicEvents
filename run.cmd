bd0877cf872eec50388817362bbed3ef02964520
Pierre BULGARE <pierre.bulgare@gmail.com>
2025-05-14 18:07:08 +0200

    Correction encodage des script cmd

--- run.cmd
+++ run.cmd
@@ -2,16 +2,16 @@
 chcp 65001 >nul
 setlocal enabledelayedexpansion
 
-:: V√©rifier que Python est install√©
+:: VÇrifier que Python est installÇ
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Python n'est pas install√© sur cet ordinateur.
+    echo Python n'est pas installÇ sur cet ordinateur.
     echo Veuillez installer Python avant de continuer.
     start https://www.python.org/downloads/
     exit /b 1
 )
 
-:: R√©cup√©rer la version de Python
+:: RÇcupÇrer la version de Python
 for /f "tokens=2 delims= " %%v in ('python --version') do set py_version=%%v
 
 :: Extraire les versions majeure et mineure
@@ -20,36 +20,36 @@ for /f "tokens=1,2 delims=." %%a in ("%py_version%") do (
     set minor=%%b
 )
 
-:: V√©rifier que la version est Python3 est install√©e
+:: VÇrifier que la version est Python3 est installÇe
 if not "!major!"=="3" (
     echo Python !py_version! n'est pas compatible avec cette application. Veuillez installer Python 3.
     start https://www.python.org/downloads/
     exit /b 1
 )
 
-:: V√©rifier que la version 3.9 ou sup√©rieure est install√©e
+:: VÇrifier que la version 3.9 ou supÇrieure est installÇe
 if !minor! lss 9 (
-    echo Python !py_version! n'est pas compatible avec cette application. Veuillez installer Python 3.9 ou sup√©rieur.
+    echo Python !py_version! n'est pas compatible avec cette application. Veuillez installer Python 3.9 ou supÇrieur.
     start https://www.python.org/downloads/
     exit /b 1
 )
 
-:: V√©rifier que pip est install√©
+:: VÇrifier que pip est installÇ
 pip --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo pip n'est pas install√© sur cet ordinateur.
+    echo pip n'est pas installÇ sur cet ordinateur.
     echo Veuillez installer pip avant de continuer.
     start https://pip.pypa.io/en/stable/installation/
     exit /b 1
 )
 
-:: V√©rifier s'il existe un environnement virtuel
+:: VÇrifier s'il existe un environnement virtuel
 if not exist "venv" (
     echo Il n'y a pas d'environnement virtuel.
-    echo Cr√©ation de l'environnement virtuel...
+    echo CrÇation de l'environnement virtuel...
     python -m venv venv
     if %errorlevel% neq 0 (
-        echo √âchec de la cr√©ation de l'environnement virtuel.
+        echo êchec de la crÇation de l'environnement virtuel.
         exit /b 1
     )
 )
@@ -60,28 +60,28 @@ if exist "venv\Scripts\activate.bat" (
     call venv\Scripts\activate
 ) else (
     echo Impossible d'activer l'environnement virtuel.
-    echo Veuillez v√©rifier que le fichier activate.bat existe dans le r√©pertoire venv\Scripts.
+    echo Veuillez vÇrifier que le fichier activate.bat existe dans le rÇpertoire venv\Scripts.
     exit /b 1
 )
 
-:: V√©rifier si les d√©pendances sont install√©es
+:: VÇrifier si les dÇpendances sont installÇes
 if not exist "requirements.txt" (
     echo Le fichier requirements.txt est introuvable.
-    echo Veuillez vous assurer que le fichier requirements.txt est pr√©sent dans le r√©pertoire courant pour installer les d√©pendances.
+    echo Veuillez vous assurer que le fichier requirements.txt est prÇsent dans le rÇpertoire courant pour installer les dÇpendances.
     exit /b 1
 )
 
-:: Mise √† jour de pip et installation des d√©pendances
-echo V√©rification et mise √† jour de pip...
+:: Mise Ö jour de pip et installation des dÇpendances
+echo VÇrification et mise Ö jour de pip...
 python -m pip install --upgrade pip --quiet
 if %errorlevel% neq 0 (
-    echo √âchec de la mise √† jour de pip.
+    echo êchec de la mise Ö jour de pip.
     exit /b 1
 )
-echo V√©rification et installation des d√©pendances...
+echo VÇrification et installation des dÇpendances...
 pip install -r requirements.txt --quiet
 if %errorlevel% neq 0 (
-    echo √âchec de l'installation des d√©pendances.
+    echo êchec de l'installation des dÇpendances.
     exit /b 1
 )
 
@@ -89,6 +89,6 @@ if %errorlevel% neq 0 (
 echo Lancement de l'application...
 python run.py
 if %errorlevel% neq 0 (
-    echo √âchec du lancement de l'application.
+    echo êchec du lancement de l'application.
     exit /b 1
 )
